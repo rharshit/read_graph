dbf497e58dfeb40e1288c24037afdd3a8e0a0b96
rharshit <rharshit98@gmail.com>
2019-04-08 12:35:53 +0530

    Use gray image for faster processing

--- graph.py
+++ graph.py
@@ -4,11 +4,6 @@ import copy
 import extract_graph
 import get_graph_dimentions
 
-
-def pixel_val(p):
-    return sum(p)
-
-
 img = cv2.imread('sample_graph.jpg')
 graph = extract_graph.extract(img)
 # cv2.imshow('graph', graph)
@@ -23,17 +18,20 @@ x_domain, y_domain, left, right, top, bottom = get_graph_dimentions.get_dim(img)
 readings = {}
 xy = {}
 
+gray = cv2.cvtColor(graph, cv2.COLOR_BGR2GRAY)
+
 for time in range(x_domain):
     bpm = 0
     x = int((time / x_domain) * (right - left))
-    col = list(graph[:, x])
-    pixel = min(col, key=pixel_val)
+    col = list(gray[:, x])
+    pixel = min(col)
     for y in range(len(col)):
-        if col[y][0] == pixel[0] and col[y][1] == pixel[1] and col[y][2] == pixel[2]:
+        if col[y] == pixel:
             bpm = 50 + ((bottom - y) / (bottom - top)) * y_domain
             readings[time] = bpm
             xy[x] = y
 
+# Display points
 for x, y in xy.items():
     cv2.circle(graph, (x, y), 2, (0, 255, 0))
 
