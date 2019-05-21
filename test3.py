256d6f12c93c3894f4adff6c33b53dc047ceefe8
rharshit <rharshit98@gmail.com>
2019-05-21 17:53:36 +0530

    Update prototype

--- test3.py
+++ test3.py
@@ -3,6 +3,49 @@ import numpy as np
 import glob, os
 import copy
 
+
+def denoise(mask, kernel_size):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    # cls = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    # # cv2.imshow('cls', cls)
+    # # cv2.waitKey(0)
+    # kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    # opn = cv2.morphologyEx(cls, cv2.MORPH_OPEN, kernel)
+    # cv2.imshow('opn', opn)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    dilate = cv2.dilate(mask, kernel, iterations=1)
+    # cv2.imshow('dilate', dilate)
+    # cv2.waitKey(0)
+    erode = cv2.erode(dilate, kernel, iterations=2)
+    # cv2.imshow('erode', erode)
+    # cv2.waitKey(0)
+    dilate = cv2.dilate(erode, kernel, iterations=1)
+    # cv2.imshow('dilate1', dilate)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return dilate
+
+
+
+def threshold(hsv, bgr, thresh):
+    lower = np.array([0, 0, 0])
+    upper = np.array([255, 50, thresh])
+
+    lower_rgb = np.array([0, 0, 0])
+    upper_rgb = np.array([thresh, thresh, thresh])
+
+    mask1 = cv2.inRange(hsv, lower, upper)
+    mask2 = cv2.inRange(bgr, lower_rgb, upper_rgb)
+    # cv2.imshow("mask1", mask1)
+    # cv2.waitKey(0)
+    # cv2.imshow("mask2", mask2)
+    # cv2.waitKey(0)
+
+    mask = cv2.bitwise_and(mask1, mask2)
+    return mask
+
 flist = glob.glob("graphs/*_*.jpg")
 flist.sort()
 
@@ -59,27 +102,15 @@ for file in flist:
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
-    hls = cv2.cvtColor(blur, cv2.COLOR_BGR2HLS)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
-    thresh = 140
+    mask = None
+    # for thresh in range(50, 200, 10):
+    #     mask = threshold(hsv, blur_chan, thresh)
+    #     cv2.imshow('mask: '+str(thresh), cv2.pyrDown(mask))
+    #     cv2.waitKey(0)
 
-    lower = np.array([0, 0, 0])
-    upper = np.array([255, thresh, 50])
-
-    lower_rgb = np.array([0, 0, 0])
-    upper_rgb = np.array([thresh, thresh, thresh])
-
-    mask1 = cv2.inRange(hls, lower, upper)
-    mask2 = cv2.inRange(blur_chan, lower_rgb, upper_rgb)
-    # cv2.imshow("mask1", mask1)
-    # cv2.waitKey(0)
-    # cv2.imshow("mask2", mask2)
-    # cv2.waitKey(0)
-
-    mask = cv2.bitwise_and(mask1, mask2)
-    # cv2.imshow('mask', mask)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    mask = threshold(hsv, blur_chan, 160)
 
     # erode_size, dilate_size = 3, 5
     #
@@ -103,20 +134,23 @@ for file in flist:
     # # cv2.imshow("dilate", dilate_v)
     # # cv2.waitKey(0)
 
-    kernel_size = 1
-    kernel = np.ones((kernel_size, kernel_size), np.uint8)
-    opn = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-    # cv2.imshow('opn', opn)
-    # cv2.waitKey(0)
+    new_mask = copy.deepcopy(mask)
+    # for size in range(1,15):
+    #     new_mask = denoise(mask, size)
+    #     cv2.imshow("size: "+str(size), new_mask)
+    #     cv2.waitKey(0)
+    #
+    new_mask = denoise(mask, 2)
 
     bg = np.ones((w, h, 3), np.uint8)
-    bg = cv2.bitwise_not(bg, mask=~mask)
+    bg = cv2.bitwise_not(bg, mask=~new_mask)
     # cv2.imshow('bg', bg)
     # cv2.waitKey(0)
 
-    res = cv2.bitwise_or(img, img, mask=mask)
+    res = cv2.bitwise_or(img, img, mask=new_mask)
     res = cv2.bitwise_or(res, bg)
 
-    # cv2.imshow("res", res)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    cv2.imshow('img', cv2.pyrDown(img))
+    cv2.imshow("res", cv2.pyrDown(res))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
